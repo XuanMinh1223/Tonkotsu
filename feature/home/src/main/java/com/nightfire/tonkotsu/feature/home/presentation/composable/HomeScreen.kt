d1fe024a2ac470496c8c4d67767a298d421ff382
khng1 <khng101@gmail.com>
2025-05-27 23:59:13 -0700

    Optimized Imports

--- feature/home/src/main/java/com/nightfire/tonkotsu/feature/home/presentation/composable/HomeScreen.kt
+++ feature/home/src/main/java/com/nightfire/tonkotsu/feature/home/presentation/composable/HomeScreen.kt
@@ -1,25 +1,17 @@
 // feature/home/src/main/java/com/nightfire.tonkotsu.feature.home.presentation.composable/HomeScreen.kt
 package com.nightfire.tonkotsu.feature.home.presentation.composable
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
