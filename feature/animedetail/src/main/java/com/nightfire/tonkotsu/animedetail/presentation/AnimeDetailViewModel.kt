7de4b0005a83949226606616e5101dd0881167bf
Khang Vu <khng101@gmail.com>
2025-06-12 15:01:00 -0700

    Fetches characters in AnimeDetailViewModel
    This commit introduces the functionality to fetch and display characters related to an anime in the `AnimeDetailViewModel`.
    
    Key changes:
    - Added `GetCharactersUseCase` dependency to the ViewModel.
    - Introduced `_animeCharactersState` and `animeCharactersState` (StateFlow) to manage the UI state of character data.
    - Implemented `getCharacters(animeId: Int)` function to fetch characters using the use case and update the state.
    - `getCharacters` is now called after successfully fetching anime details.

--- feature/animedetail/src/main/java/com/nightfire/tonkotsu/animedetail/presentation/AnimeDetailViewModel.kt
+++ feature/animedetail/src/main/java/com/nightfire/tonkotsu/animedetail/presentation/AnimeDetailViewModel.kt
@@ -6,8 +6,10 @@ import com.nightfire.tonkotsu.core.common.Resource
 import com.nightfire.tonkotsu.core.common.UiState
 import com.nightfire.tonkotsu.core.domain.model.AnimeDetail
 import com.nightfire.tonkotsu.core.domain.model.AnimeEpisode
+import com.nightfire.tonkotsu.core.domain.model.Character
 import com.nightfire.tonkotsu.core.domain.usecase.GetAnimeDetailUseCase
 import com.nightfire.tonkotsu.core.domain.usecase.GetAnimeEpisodesUseCase
+import com.nightfire.tonkotsu.core.domain.usecase.GetCharactersUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -19,12 +21,17 @@ import javax.inject.Inject
 class AnimeDetailViewModel @Inject constructor(
     private val getAnimeDetailUseCase: GetAnimeDetailUseCase,
     private val getAnimeEpisodesUseCase: GetAnimeEpisodesUseCase,
+    private val getCharactersUseCase: GetCharactersUseCase,
 ) : ViewModel() {
+
     private val _animeDetailState = MutableStateFlow<UiState<AnimeDetail>>(UiState.loading())
     val animeDetailState : StateFlow<UiState<AnimeDetail>> = _animeDetailState
 
     private val _animeEpisodesState = MutableStateFlow<UiState<List<AnimeEpisode>>>(UiState.loading())
     val animeEpisodesState: StateFlow<UiState<List<AnimeEpisode>>> = _animeEpisodesState
+
+    private val _animeCharactersState = MutableStateFlow<UiState<List<Character>>>(UiState.loading())
+    val animeCharactersState: StateFlow<UiState<List<Character>>> = _animeCharactersState
 
     fun getAnimeDetail(id: Int) {
         getAnimeDetailUseCase(id).onEach { result ->
@@ -39,6 +46,7 @@ class AnimeDetailViewModel @Inject constructor(
 
                     if (result.data != null) {
                         getAnimeEpisodes(id)
+                        getCharacters(id)
                     }
                 }
                 is Resource.Error -> {
@@ -65,6 +73,25 @@ class AnimeDetailViewModel @Inject constructor(
                 is Resource.Error -> {
                     _animeEpisodesState.value = UiState.error(
                         message = result.message ?: "An unexpected error occurred loading Episodes.",
+                        data = result.data
+                    )
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+
+    private fun getCharacters(animeId: Int) {
+        getCharactersUseCase(animeId).onEach { result ->
+            when (result) {
+                is Resource.Loading -> {
+                    _animeCharactersState.value = UiState.loading()
+                }
+                is Resource.Success -> {
+                    _animeCharactersState.value = UiState.loading()
+                }
+                is Resource.Error -> {
+                    _animeCharactersState.value = UiState.error(
+                        message = result.message ?: "An unexpected error occurred loading characters.",
                         data = result.data
                     )
                 }
